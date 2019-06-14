f19e37594a085b9447fcab4e26a347a393f65d03
Dirk Chang <dirk@kooiot.com>
2019-06-14 11:48:02 +0800

    create script to make the release tar.gz

---
+++ .travis_pack.sh
@@ -0,0 +1,23 @@
+rm -rf __install
+mkdir __install
+
+echo ${TRAVIS_JOB_ID} > __install/version
+echo ${TRAVIS_JOB_NUMBER} >> __install/version
+
+# Copy files
+cp -r lualib __install/lualib
+cp -r luaclib __install/luaclib
+cp -r service __install/service
+cp -r cservice __install/cservice
+cp README.md __install/
+cp HISTORY.md __install/
+cp LICENSE __install/
+cp skynet __install/
+cd __install/
+ln -s ../freeioe ./ioe
+ln -s /var/log ./logs
+cd - > /dev/null
+
+cd __install
+tar czvf ../skynet_release.tar.gz * > /dev/null
+md5sum ../skynet_release.tar.gz > ../skynet_release.tar.gz.md5
