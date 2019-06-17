3c5ad04ed0e3441e3fb9e1dcc9d6f7505c63eb6e
Dirk Chang <dirk@kooiot.com>
2019-06-17 11:21:13 +0800

    added missing rs232.lua in travis pack script

--- .travis_pack.sh
+++ .travis_pack.sh
@@ -6,6 +6,7 @@ echo ${TRAVIS_JOB_NUMBER} >> __install/version
 
 # Copy files
 cp -r lualib __install/lualib
+cp 3rd/librs232/bindings/lua/rs232.lua __install/lualib
 cp -r luaclib __install/luaclib
 cp -r service __install/service
 cp -r cservice __install/cservice
@@ -13,7 +14,9 @@ cp README.md __install/
 cp HISTORY.md __install/
 cp LICENSE __install/
 cp skynet __install/
+
 cd __install/
+
 ln -s ../freeioe ./ioe
 ln -s /var/log ./logs
 cd - > /dev/null
